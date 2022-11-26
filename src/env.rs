6e5c711f2e6feb059bfb6dd7cafab3375a26bbe8
Paul Horn <dev@knutwalker.engineer>
2022-11-26 15:47:59 +0100

    Add environment tests

--- src/env.rs
+++ src/env.rs
@@ -30,3 +30,64 @@ impl<'a> Environment<'a> {
             .map(|idx| &mut self.values[idx])
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_get_on_empty() {
+        let env = Environment::default();
+        assert_eq!(env.get("foo"), None);
+    }
+
+    #[test]
+    fn test_get_after_define() {
+        let mut env = Environment::default();
+        env.define("foo", Value::Number(42.0));
+        assert_eq!(env.get("foo"), Some(&Value::Number(42.0)));
+    }
+
+    #[test]
+    fn test_get_mut_on_empty() {
+        let mut env = Environment::default();
+        assert_eq!(env.get_mut("foo"), None);
+    }
+
+    #[test]
+    fn test_get_mut_after_define() {
+        let mut env = Environment::default();
+        env.define("foo", Value::Number(42.0));
+        assert_eq!(env.get_mut("foo"), Some(&mut Value::Number(42.0)));
+    }
+
+    #[test]
+    fn test_assign_on_empty() {
+        let mut env = Environment::default();
+        assert_eq!(env.assign("foo", Value::Number(42.0)), None);
+        assert_eq!(env.get("foo"), None);
+    }
+
+    #[test]
+    fn test_assign_after_define() {
+        let mut env = Environment::default();
+        env.define("foo", Value::Number(42.0));
+        assert_eq!(
+            env.assign("foo", Value::Number(24.0)),
+            Some(Value::Number(42.0))
+        );
+        assert_eq!(env.get("foo"), Some(&Value::Number(24.0)));
+    }
+
+    #[test]
+    fn test_get_mut_after_assign() {
+        let mut env = Environment::default();
+        env.define("foo", Value::Number(42.0));
+        assert_eq!(
+            env.assign("foo", Value::Number(24.0)),
+            Some(Value::Number(42.0))
+        );
+        assert_eq!(env.get_mut("foo"), Some(&mut Value::Number(24.0)));
+    }
+}
