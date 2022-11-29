eb5d0bd8419d61574a1a4737f8513825cfc67c91
Paul Horn <dev@knutwalker.engineer>
2022-11-29 19:52:14 +0100

    Fix parser benches

--- benches/parser.rs
+++ benches/parser.rs
@@ -12,5 +12,15 @@ fn crox_parser(b: &mut Bencher) {
     let source = crox::scan(input);
     let tokens = source.scan_all().unwrap();
     b.bytes = input.len() as u64;
-    b.iter(|| crox::parser(source, tokens.iter().copied()).collect::<Vec<_>>());
+    b.iter(|| crox::stmt_parser(source, tokens.iter().copied()).collect::<Vec<_>>());
+}
+
+#[bench]
+fn crox_scanner_then_parser(b: &mut Bencher) {
+    let input = include_str!("../tests/classes.crox");
+    let source = crox::scan(input);
+    b.bytes = input.len() as u64;
+    b.iter(|| {
+        crox::stmt_parser(source, source.into_iter().map(Result::unwrap)).collect::<Vec<_>>()
+    });
 }
