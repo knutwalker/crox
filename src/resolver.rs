74b0c91a6fce30aeb8c9f3b18fb14d863a9627d1
Paul Horn <dev@knutwalker.engineer>
2023-01-06 15:48:03 +0100

    Make resolver infallible

--- src/resolver.rs
+++ src/resolver.rs
@@ -1,7 +1,4 @@
-use crate::{
-    CroxErrorKind, Environment, Expr, ExprNode, ExpressionRule, Result, StatementRule, Stmt,
-    StmtNode, Var,
-};
+use crate::{Environment, Expr, ExprNode, ExpressionRule, StatementRule, Stmt, StmtNode, Var};
 use std::marker::PhantomData;
 
 #[derive(Clone, Debug)]
@@ -24,17 +21,16 @@ impl<'a> Default for Resolver<'a> {
 }
 
 impl<'a> Resolver<'a> {
-    pub fn eval_stmts_in_scope(env: &Environment<'a, ()>, stmts: &[StmtNode<'a>]) -> Result<()> {
-        stmts.iter().try_for_each(|stmt| -> Result<()> {
-            Self::eval_stmt(env, &stmt.item)?;
-            Ok(())
-        })
+    pub fn eval_stmts_in_scope(env: &Environment<'a, ()>, stmts: &[StmtNode<'a>]) {
+        stmts.iter().for_each(|stmt| {
+            Self::eval_stmt(env, &stmt.item);
+        });
     }
 
-    pub fn eval_stmt(env: &Environment<'a, ()>, stmt: &Stmt<'a>) -> Result<()> {
+    pub fn eval_stmt(env: &Environment<'a, ()>, stmt: &Stmt<'a>) {
         match &stmt {
             Stmt::Expression { expr } => {
-                Self::eval_expr(env, expr)?;
+                Self::eval_expr(env, expr);
             }
             Stmt::Function(func) => {
                 env.define(func.name.item, ());
@@ -43,58 +39,55 @@ impl<'a> Resolver<'a> {
                         env.define(param.item, ());
                     }
                     Self::eval_stmts_in_scope(env, &func.fun.body)
-                })?;
+                });
             }
             Stmt::If {
                 condition,
                 then_,
                 else_,
             } => {
-                Self::eval_expr(env, condition)?;
-                Self::eval_stmt(env, &then_.item)?;
+                Self::eval_expr(env, condition);
+                Self::eval_stmt(env, &then_.item);
                 if let Some(else_) = else_ {
-                    Self::eval_stmt(env, &else_.item)?;
+                    Self::eval_stmt(env, &else_.item);
                 }
             }
             Stmt::Print { expr } => {
-                Self::eval_expr(env, expr)?;
+                Self::eval_expr(env, expr);
             }
             Stmt::Return { expr } => {
                 if let Some(expr) = expr.as_ref() {
-                    Self::eval_expr(env, expr)?;
+                    Self::eval_expr(env, expr);
                 }
             }
             Stmt::Var { name, initializer } => {
                 // The book splits this into two steps to make shadowing an error.
                 // In Crox, we allow shadowing.
                 if let Some(init) = initializer.as_ref() {
-                    Self::eval_expr(env, init)?;
+                    Self::eval_expr(env, init);
                 }
                 env.define(name.item, ());
             }
             Stmt::While { condition, body } => {
-                Self::eval_expr(env, condition)?;
-                Self::eval_stmt(env, &body.item)?;
+                Self::eval_expr(env, condition);
+                Self::eval_stmt(env, &body.item);
             }
             Stmt::Block { stmts } => {
-                env.run_with_new_scope(|env| Self::eval_stmts_in_scope(env, stmts))?;
+                env.run_with_new_scope(|env| Self::eval_stmts_in_scope(env, stmts));
             }
         };
-        Ok(())
     }
 
-    pub fn eval_expr(env: &Environment<'a, ()>, expr: &ExprNode<'a>) -> Result<()> {
-        let span = expr.span;
+    pub fn eval_expr(env: &Environment<'a, ()>, expr: &ExprNode<'a>) {
         match &*expr.item {
             Expr::Literal(_) => {}
             Expr::Var(Var {
                 name,
                 resolved_scope,
             }) => {
-                let scope = env
-                    .scope_of(name)
-                    .map_err(|e| CroxErrorKind::from(e).at(span))?;
-                resolved_scope.set(Some(scope));
+                if let Ok(scope) = env.scope_of(name) {
+                    resolved_scope.set(Some(scope));
+                }
             }
             Expr::Fun(func) => {
                 env.run_with_new_scope(|env| {
@@ -102,50 +95,46 @@ impl<'a> Resolver<'a> {
                         env.define(param.item, ());
                     }
                     Self::eval_stmts_in_scope(env, &func.body)
-                })?;
+                });
             }
             Expr::Assignment { var, value } => {
-                Self::eval_expr(env, value)?;
-                let scope = env
-                    .scope_of(var.name)
-                    .map_err(|e| CroxErrorKind::from(e).at(span))?;
-                var.resolved_scope.set(Some(scope));
+                Self::eval_expr(env, value);
+                if let Ok(scope) = env.scope_of(var.name) {
+                    var.resolved_scope.set(Some(scope));
+                }
             }
             Expr::Unary { expr, .. } => {
-                Self::eval_expr(env, expr)?;
+                Self::eval_expr(env, expr);
             }
             Expr::Logical { lhs, rhs, .. } => {
-                Self::eval_expr(env, lhs)?;
-                Self::eval_expr(env, rhs)?;
+                Self::eval_expr(env, lhs);
+                Self::eval_expr(env, rhs);
             }
             Expr::Binary { lhs, rhs, .. } => {
-                Self::eval_expr(env, lhs)?;
-                Self::eval_expr(env, rhs)?;
+                Self::eval_expr(env, lhs);
+                Self::eval_expr(env, rhs);
             }
             Expr::Call { callee, arguments } => {
-                Self::eval_expr(env, callee)?;
-                arguments
-                    .iter()
-                    .try_for_each(|arg| Self::eval_expr(env, arg))?;
+                Self::eval_expr(env, callee);
+                arguments.iter().for_each(|arg| Self::eval_expr(env, arg));
             }
             Expr::Group { expr } => {
-                Self::eval_expr(env, expr)?;
+                Self::eval_expr(env, expr);
             }
         };
-        Ok(())
     }
 }
 
 impl<'a> Resolver<'a> {
-    pub fn eval_own_stmts_in_scope(&self, stmts: &[StmtNode<'a>]) -> Result<()> {
+    pub fn eval_own_stmts_in_scope(&self, stmts: &[StmtNode<'a>]) {
         Self::eval_stmts_in_scope(&self.env, stmts)
     }
 
-    pub fn eval_own_stmt(&self, stmt: &Stmt<'a>) -> Result<()> {
+    pub fn eval_own_stmt(&self, stmt: &Stmt<'a>) {
         Self::eval_stmt(&self.env, stmt)
     }
 
-    pub fn eval_own_expr(&self, expr: &ExprNode<'a>) -> Result<()> {
+    pub fn eval_own_expr(&self, expr: &ExprNode<'a>) {
         Self::eval_expr(&self.env, expr)
     }
 }
@@ -167,14 +156,14 @@ impl<'a, R, I> StreamResolver<'a, R, I> {
     }
 }
 
-pub fn stmt_resolver<'a, I>(tokens: I) -> impl Iterator<Item = Result<StmtNode<'a>>>
+pub fn stmt_resolver<'a, I>(tokens: I) -> impl Iterator<Item = StmtNode<'a>>
 where
     I: IntoIterator<Item = StmtNode<'a>>,
 {
     StreamResolver::<StatementRule, _>::new(tokens.into_iter())
 }
 
-pub fn expr_resolver<'a, I>(tokens: I) -> impl Iterator<Item = Result<ExprNode<'a>>>
+pub fn expr_resolver<'a, I>(tokens: I) -> impl Iterator<Item = ExprNode<'a>>
 where
     I: IntoIterator<Item = ExprNode<'a>>,
 {
@@ -186,27 +175,25 @@ where
     R: ResolverRule,
     I: Iterator<Item = R::Input<'a>>,
 {
-    type Item = Result<I::Item>;
+    type Item = I::Item;
 
     fn next(&mut self) -> Option<Self::Item> {
         let input = self.input.next()?;
-        match R::resolve(&mut self.resolver, &input) {
-            Ok(()) => Some(Ok(input)),
-            Err(e) => Some(Err(e)),
-        }
+        R::resolve(&mut self.resolver, &input);
+        Some(input)
     }
 }
 
 pub trait ResolverRule: Sized {
     type Input<'a>;
 
-    fn resolve<'a>(interpreter: &mut Resolver<'a>, input: &Self::Input<'a>) -> Result<()>;
+    fn resolve<'a>(interpreter: &mut Resolver<'a>, input: &Self::Input<'a>);
 }
 
 impl ResolverRule for ExpressionRule {
     type Input<'a> = ExprNode<'a>;
 
-    fn resolve<'a>(interpreter: &mut Resolver<'a>, input: &Self::Input<'a>) -> Result<()> {
+    fn resolve<'a>(interpreter: &mut Resolver<'a>, input: &Self::Input<'a>) {
         interpreter.eval_own_expr(input)
     }
 }
@@ -214,7 +201,7 @@ impl ResolverRule for ExpressionRule {
 impl ResolverRule for StatementRule {
     type Input<'a> = StmtNode<'a>;
 
-    fn resolve<'a>(interpreter: &mut Resolver<'a>, input: &Self::Input<'a>) -> Result<()> {
+    fn resolve<'a>(interpreter: &mut Resolver<'a>, input: &Self::Input<'a>) {
         interpreter.eval_own_stmt(&input.item)
     }
 }
